86ddbf205cee5e2296445a09b259e24dc8d689da
Bayram Uslu <bayramuslu9741@gmail.com>
2023-03-20 17:21:35 +0300

    Added : level data

---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,26 @@
+namespace ConnectFoods.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "level", menuName = "LevelData", order = 0)]
+    public class LevelData : ScriptableObject
+    {
+        [Serializable]
+        public struct TargetObjective
+        {
+            public string name;
+            public int count;
+        }
+
+        public int level;
+        public int totalMove;
+        public int rowCount;
+        public int columnCount;
+        [Tooltip("Target Names : apple - leaf - banana - blob - baloon")] 
+        public List<TargetObjective> targets;
+        public bool isCompleted;
+        public int highScore;
+    }
+}
